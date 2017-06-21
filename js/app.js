e90aa9b540c0d030dbd3e7ef92be518feec6f911
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-06-21 09:35:34 -0400

    WIP

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
 "use strict";
 
+
+
 (function(){
+
+  let WDInstagramData = [
+    {photo_url:"https://vetstreet.brightspotcdn.com/dims4/default/edb51df/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F19%2F716f20a7f711e0a0d50050568d634f%2Ffile%2FPomeranian-2-645mk062811.jpg", author: "Sonny", body: "My prince <3"},
+    {photo_url:"https://s-media-cache-ak0.pinimg.com/736x/e4/7c/e5/e47ce5b12dd50743b35a008d09630d12.jpg", author: "Danielle", body: "The puppsters!"},
+    {photo_url:"https://s-media-cache-ak0.pinimg.com/736x/cf/b7/a2/cfb7a23b99bded240bb685494bfe8525.jpg", author: "Jenny", body: "Sleeping time zzzzzz."}
+  ]
+  
   angular
   .module("wdinstagram", [])
+  // .config("$stateProvider", RouterFunction)
+  .controller("WDInstragramIndexController", WDInstragramIndexControllerFunction)
+  // .controller("WDInstragramShowController", ["$stateParams", WDInstragramIndexControllerFunction])
+
+  function WDInstragramIndexControllerFunction() {
+    this.instagrams = WDInstagramData
+  }
 
 
 
@@ -10,6 +26,4 @@
 
 
 
-
-  
 })();
